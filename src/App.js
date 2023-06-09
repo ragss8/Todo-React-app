f44306bfe92e091a161a3fad803c376e633eee60
ragss8 <96374035+ragss8@users.noreply.github.com>
2023-06-09 19:01:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,94 +1,90 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 
 function TodoApp() {
-  const [todos, setTodos] = useState([]);  //set the todolist and initialize it as empty
-  const [inputValue, setInputValue] = useState(""); //set the inputValue to null
-  const [completedTodo, setCompletedTodo] = useState([]);
-  const [filter, setFilter] = useState("all");
+  const [todos, setTodos] = useState([]); // set the todolist and initialize it as empty
+  const [inputValue, setInputValue] = useState(""); // set the inputValue to null
+  const [filter, setFilter] = useState("all"); // set the initial filter value to "all"
 
   const handleInputChange = (event) => {
-    setInputValue(event.target.value);   //set the input with the InputValue
+    setInputValue(event.target.value); // set the input with the inputValue
   };
 
-  const handelAddTodo = () => {
+  const handleAddTodo = () => {
     if (inputValue.trim() !== "") {
       const newTodo = {
+        // create a new todo object
+        id: Date.now(), // assign a unique id
         text: inputValue,
-        completed: false,         //here complete is set to false as it is not set
+        completed: false, // set completed to false initially
       };
-      setTodos([...todos, newTodo]);  //spread list is used here to expand the todo added to the todos
-      setInputValue(''); //reset the inputValue to null with this function
+      setTodos((prevTodos) => [...prevTodos, newTodo]); // add the new todo to the todos list
+      setInputValue(""); // reset the inputValue to an empty string
     }
   };
 
-  const handleDeleteTodo = (index) => {
-    const updatedTodos = [...todos];
-    updatedTodos.splice(index, 1);
-    setTodos(updatedTodos);
-  };
-
-  const handleToggleTodo = (index) => {
-    const updatedTodos = [...todos];      //we just mutate and put the todos into the updated Todos list
-    updatedTodos[index] = {
-      ...updatedTodos[index],  //Creates a new object by spreading the properties of the existing todo item and updating the completed property 
-      completed: !updatedTodos[index].completed, //We toggle the completed property by negating its current value using !.
-    };
-
-    if (updatedTodos[index].completed) {   //if that index of updatedTodos list is checked it is inserted into setCompletedTodo list 
-      setCompletedTodo([...completedTodo, updatedTodos[index]]);  //we add it to the completedTodos array by spreading the existing completed todos and adding the updated todo item.
-    } else {
-      const filteredcompletedTodos = completedTodo.filter(
-        (todo) => todo !== updatedTodos[index]
-      );
-      setCompletedTodo(filteredcompletedTodos);
+  const handleDeleteTodo = (id) => {  // in this function we use findIndex method to find the index of the todo with specified id
+    const index = todos.findIndex((todo) => todo.id === id);
+    if(index !== -1){
+      const updatedTodos = [...todos];
+      updatedTodos.splice(index , 1);
+      setTodos(updatedTodos);
     }
-    setTodos(updatedTodos);
   };
+
+  // React hook that memoizes the callback function. It ensures that the callback is only recreated if any of its dependencies change. 
+  const handleToggleTodo = useCallback((id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed }; // toggle the completed property of the todo
+        }
+        return todo;
+      })
+    );
+  }, []); //an empty dependency array ([]) is provided, indicating that the callback doesn't depend on any external variables or props.
 
   const handleFilterChange = (filterType) => {
-    setFilter(filterType);
+    setFilter(filterType); // update the filter value
   };
+
+  const filteredTodos = todos.filter((todo) => {
+    if (filter === "all") {
+      return true; // show all todos
+    } else if (filter === "completed") {
+      return todo.completed; // show only completed todos
+    } else if (filter === "active") {
+      return !todo.completed; // show only active todos
+    }
+    return false;
+  });
 
   return (
     <div>
       <h1>TodoApp</h1>
       <input type="text" value={inputValue} onChange={handleInputChange} />
-      <button onClick={handelAddTodo}>Add Todo</button>
+      <button onClick={handleAddTodo}>Add Todo</button>
       <div>
         <button onClick={() => handleFilterChange("all")}>All</button>
-        <button onClick={() => handleFilterChange("completed")}>completed</button>
+        <button onClick={() => handleFilterChange("completed")}>Completed</button>
         <button onClick={() => handleFilterChange("active")}>Active</button>
       </div>
       <ul>
-        {todos.map((todo, index) => {
-          if (
-            filter === "all" ||
-            (filter === "completed" && todo.completed) ||
-            (filter === "active" && !todo.completed)
-          ) {
-            return (
-              <li key={index}>
-                <input
-                  type="checkbox"
-                  checked={todo.completed}
-                  onChange={() => handleToggleTodo(index)}
-                />
-                <span
-                  style={{
-                    textDecoration: todo.completed ? "line-through" : "none",
-                  }}
-                >
-                  {todo.text}
-                </span>
-                <button onClick={() => handleDeleteTodo(index)}>Delete</button>
-              </li>
-            );
-          }
-          return null;
-        })}
+        {filteredTodos.map((todo) => (
+          <li key={todo.id}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => handleToggleTodo(todo.id)}
+            />
+            <span style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
+              {todo.text}
+            </span>
+            <button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
       </ul>
     </div>
   );
 }
 
-export default TodoApp
+export default TodoApp;
