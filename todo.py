081d0928b1931a2dfa3b93dfe6a8c1b793bc1096
ragss8 <96374035+ragss8@users.noreply.github.com>
2023-06-19 14:12:33 +0530

    Update todo.py

--- todo.py
+++ todo.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI,HTTPException,APIRouter
+from fastapi import FastAPI,HTTPException, Body
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 from pymongo import MongoClient
+from fastapi.responses import JSONResponse
+from bson import ObjectId
 from typing import List
 
-app = FastAPI()
+app = FastAPI(debug= True)
 
 mongodb_uri ='mongodb+srv://raghugaikwad8641:Raghugaikwad8@userinfo.d4n8sns.mongodb.net/?retryWrites=true&w=majority'
 port = 8000
@@ -12,77 +15,150 @@ db = client.get_database('todoapp')
 user_collection = db.todos
 
 
-
-# Model for a todo item
 class TodoItem(BaseModel):
-    id: int
-    text: str
+    item_text: str
     completed: bool
 
-# Endpoint to add a new todo item
 @app.post("/todos")
-async def add_todo_item(item: TodoItem):
-    todo_dict = item.dict()
-    todo_dict["completed"] = False
-    user_collection.insert_one(todo_dict)
-    return item
+async def create_todo(todo: TodoItem):
+    new_todo = dict(todo)
+    inserted_id = user_collection.insert_one(new_todo).inserted_id
+    new_todo["_id"] = str(inserted_id)
+    return {"todo_id": str(inserted_id), "item_text": new_todo["item_text"], "message": "Todo created successfully"}
+
+
+@app.delete("/todos/{todo_id}")
+async def delete_todo(todo_id: str):
+    try:
+        todo_object_id = ObjectId(todo_id)
+        
+        # Retrieve the todo item from user_collection
+        todo_item = user_collection.find_one({"_id": todo_object_id})
+
+        if todo_item:
+            # Retrieve the deleted todo item's ID
+            deleted_todo_id = str(todo_item["_id"])
+
+            # Delete the todo item
+            result = user_collection.delete_one({"_id": todo_object_id})
+
+            if result.deleted_count:
+                return {"message": "Todo deleted successfully", "deleted_todo_id": deleted_todo_id}
+            else:
+                return {"message": "Todo not found"}
+        else:
+            return {"message": "Todo not found"}
+    except Exception as e:
+        return {"message": f"Error deleting todo: {str(e)}"}
+    
+""" @app.get("/todos/{todo_id}")
+async def get_todo_by_id(todo_id: str):
+    todo = user_collection.find_one({"_id": ObjectId(todo_id)})
+    if todo:
+        todo_item = {
+            "todo_id": str(todo["_id"]),
+            "item_text": todo["item_text"],
+            "completed": todo["completed"]
+        }
+        return todo_item
+    else:
+        raise HTTPException(status_code=404, detail="Todo not found")  """   
+
 
 # Endpoint to edit a todo item
-@app.put("/todos/{item_id}")
-async def edit_todo_item(item_id: int, item: TodoItem):
-    query = {"id": item_id}
-    updated_item = {"$set": {"text": item.text}}
-    result = user_collection.update_one(query, updated_item)
+@app.get("/todos/{todo_id}/edit")
+async def edit_todo(todo_id: str):
+    try:
+        # Convert the provided string to a valid ObjectId
+        object_id = ObjectId(todo_id)
+        
+        # Find the todo item with the given ObjectId
+        todo = user_collection.find_one({"_id": object_id})
+        if todo is None:
+            raise HTTPException(status_code=404, detail="Todo item not found")
+        
+        # Convert the retrieved todo item to a dictionary
+        todo_dict = dict(todo)
+        todo_dict["_id"] = str(todo_dict["_id"])  # Convert _id to string
+        
+        return {"todo": todo_dict}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
-    if result.modified_count == 0:
-        raise HTTPException(status_code=404, detail="Todo item not found")
 
-    return item
 
-# Endpoint to delete a todo item
-@app.delete("/todos/{item_id}")
-async def delete_todo_item(item_id: int):
-    query = {"id": item_id}
-    result = user_collection.delete_one(query)
+@app.put("/todos/{todo_id}")
+async def update_todo_item(todo_id: str, todo: TodoItem):
+    result = user_collection.update_one(
+        {"_id": ObjectId(todo_id)},
+        {"$set": {
+            "item_text": todo.item_text,
+            "completed": todo.completed
+        }}
+    )
+    if result.modified_count > 0:
+        return {"message": "Todo updated successfully"}
+    else:
+        raise HTTPException(status_code=404, detail="Todo not found or update failed")
 
-    if result.deleted_count == 0:
-        raise HTTPException(status_code=404, detail="Todo item not found")
-
-    return {"message": "Todo item deleted"}
 
 # Endpoint to toggle the completion status of a todo item
-@app.put("/todos/{item_id}/toggle")
-async def toggle_todo_item(item_id: int):
-    query = {"id": item_id}
-    todo = user_collection.find_one(query)
+@app.put("/todos/{todo_id}/toggle")
+async def toggle_todo_status(todo_id: str):
+    try:
+        # Convert the provided string to a valid ObjectId
+        object_id = ObjectId(todo_id)
 
-    if todo is None:
-        raise HTTPException(status_code=404, detail="Todo item not found")
+        # Find the todo item with the given ObjectId
+        todo = user_collection.find_one({"_id": object_id})
+        if todo is None:
+            raise HTTPException(status_code=404, detail="Todo item not found")
 
-    completed = not todo["completed"]
-    updated_item = {"$set": {"completed": completed}}
-    user_collection.update_one(query, updated_item)
+        # Toggle the completed status of the todo item
+        new_completed_status = not todo["completed"]
+        user_collection.update_one(
+            {"_id": object_id},
+            {"$set": {"completed": new_completed_status}}
+        )
 
-    return {"message": "Todo item toggled"}
+        return {"message": "Todo item status toggled successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
 
 # Endpoint to filter todo items based on completion status
-@app.get("/todos/{status}", response_model=List[TodoItem])
-async def filter_todo_items(status: str):
-    if status == "completed":
-        query = {"completed": True}
-    elif status == "active":
-        query = {"completed": False}
-    elif status == "all":
-        query = {}
+@app.get("/todos")
+def get_filtered_todos(filter_type: str):
+    filter_query = {}
+    if filter_type == "completed":
+        filter_query = {"completed": True}
+    elif filter_type == "active":
+        filter_query = {"completed": False}
+    elif filter_type == "all":
+        pass
     else:
-        raise HTTPException(status_code=400, detail="Invalid status")
+        raise HTTPException(status_code=400 , detail="Invalid filter type")    
+    todos = user_collection.find(filter_query)
+    return list(todos)
 
-    items = user_collection.find(query)
-    todo_items = [TodoItem(**item) for item in items]
+""" @app.get("/todos")
+def get_filtered_todos(filter_type: str):
+    filtered_todos = []
 
-    return todo_items
+    if filter_type == "all":
+        filtered_todos = todos
+    elif filter_type == "completed":
+        filtered_todos = [todo for todo in todos if todo["completed"]]
+    elif filter_type == "active":
+        filtered_todos = [todo for todo in todos if not todo["completed"]]
+
+    return filtered_todos """
 
 
-
-
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
