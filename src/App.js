3be2bb24f44ba49dcd1f7c813f9a4901e514d752
ragss8 <96374035+ragss8@users.noreply.github.com>
2023-06-20 00:37:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import './App.css';
+import "./App.css";
 
 function TodoApp() {
   const [todos, setTodos] = useState([]);
@@ -12,75 +12,82 @@ function TodoApp() {
     setInputValue(event.target.value);
   };
 
-  const handleAddTodo = () => {
+  const handleAddTodo = async () => {
     if (inputValue.trim() !== "") {
       const newTodo = {
         item_text: inputValue,
         completed: false,
-        todo_id: "", // Placeholder for the todo_id
       };
-  
-      axios
-        .post("http://localhost:8000/todos", newTodo)
-        .then((response) => {
-          const addedTodo = response.data;
-          newTodo.todo_id = addedTodo.todo_id; // Update the todo_id in the newTodo object
-          setTodos((prevTodos) => [...prevTodos, newTodo]);
-          setInputValue("");
-          alert(`Todo added with ID: ${addedTodo.todo_id}`);
-        })
-        .catch((error) => {
-          if (error.response) {
-            console.log("Error", error.message);
-          }
-        });
+
+      try {
+        const response = await axios.post("http://localhost:8000/todos", newTodo);
+        const addedTodo = response.data;
+        setTodos((prevTodos) => [...prevTodos, addedTodo]);
+        setInputValue("");
+        console.log(`Todo added with ID: ${addedTodo.todo_id}`);
+      } catch (error) {
+        if (error.response) {
+          console.log("Error", error.message);
+        }
+      }
     }
   };
-  
-  
-  const handleDeleteTodo = (todo_id) => {
-    axios
-      .delete(`http://localhost:8000/todos/${todo_id}`)
-      .then((response) => {
-        console.log(response.data);
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      if (editTodo) {
+        handleUpdateTodo();
+      } else {
+        handleAddTodo();
+      }
+    }
+  };
+
+  const handleDeleteTodo = async (todo_id, item_text) => {
+    const confirmationMessage = `Are you sure you want to delete todo with ID: ${todo_id} and item text: ${item_text}?`;
+
+    if (window.confirm(confirmationMessage)) {
+      try {
+        const response = await axios.delete(`http://localhost:8000/todos/${todo_id}`);
         if (response.data.message === "Todo deleted successfully") {
           setTodos((prevTodos) =>
             prevTodos.filter((todo) => todo.todo_id !== todo_id)
           );
-          window.alert('Item deleted successfully');
+          console.log("Item deleted successfully");
         } else {
-          window.alert('Todo not found');
+          console.log("Todo not found");
         }
-      })
-      .catch((error) => {
+      } catch (error) {
         alert("Error: " + error.message);
-      });
-  };
-  
-  const handleEditTodo = async (todo_id) => {
-    try {
-      const response = await axios.get(`http://localhost:8000/todos/${todo_id}`);
-      const todoToEdit = response.data;
-  
-      setEditTodo(todoToEdit);
-    } catch (error) {
-      console.error('Error fetching todo:', error);
+      }
     }
   };
-  
+
+  const handleEditTodo = (todo_id) => {
+    setEditTodo(todo_id);
+  };
+
   const handleUpdateTodo = () => {
     if (!editTodo) {
-      console.log('No todo to update');
+      console.log("No todo to update");
       return;
     }
-  
+
+    // Find the todo with the matching todo_id
+    const todoToEdit = todos.find((todo) => todo.todo_id === editTodo);
+
+    if (!todoToEdit) {
+      console.error(`Todo with ID ${editTodo} not found`);
+      return;
+    }
+
     axios
-      .put(`http://localhost:8000/todos/${editTodo.id}`, editTodo)
+      .put(`http://localhost:8000/todos/${editTodo}`, todoToEdit)
       .then((response) => {
         const { message } = response.data;
         if (message === "Todo updated successfully") {
           const updatedTodos = todos.map((todo) =>
-            todo.id === editTodo.id ? editTodo : todo
+            todo.todo_id === editTodo ? todoToEdit : todo
           );
           setTodos(updatedTodos);
           setEditTodo(null);
@@ -93,13 +100,13 @@ function TodoApp() {
         console.error(error);
       });
   };
-  
+
   const handleToggleTodo = async (todo_id) => {
     try {
       const response = await axios.put(`http://localhost:8000/todos/${todo_id}/toggle`);
       setTodos((prevTodos) =>
         prevTodos.map((todo) => {
-          if (todo._id === todo_id) {
+          if (todo.todo_id === todo_id) {
             return { ...todo, completed: !todo.completed };
           }
           return todo;
@@ -111,17 +118,9 @@ function TodoApp() {
     }
   };
 
-  const handleFilterChange = async (filterType) => {
-    try {
-      const response = await axios.get(`http://localhost:8000/todos?filter=${filterType}`);
-      const filteredTodos = response.data;
-      setFilter(filterType);
-      setTodos(filteredTodos);
-    } catch (error) {
-      console.error(error);
-    }
+  const handleFilterChange = (filterType) => {
+    setFilter(filterType);
   };
-  
 
   const filteredTodos = todos.filter((todo) => {
     if (filter === "all") {
@@ -133,8 +132,6 @@ function TodoApp() {
     }
     return false;
   });
-  
-  
 
   return (
     <div className="body">
@@ -144,6 +141,7 @@ function TodoApp() {
           type="text"
           value={inputValue}
           onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
           style={{ textAlign: "center" }}
         />
         <button onClick={handleAddTodo}>Add Todo</button>
@@ -151,48 +149,56 @@ function TodoApp() {
       <div className="container">
         <div className="button">
           <button onClick={() => handleFilterChange("all")}>All</button>
-          <button onClick={() => handleFilterChange("completed")}>
-            Completed
-          </button>
+          <button onClick={() => handleFilterChange("completed")}>Completed</button>
           <button onClick={() => handleFilterChange("active")}>Active</button>
         </div>
         <div className="listItems" style={{ textAlign: "revert-layer" }}>
           <h2 style={{ textAlign: "center" }}>Todo-List</h2>
           <ol>
-  {filteredTodos.map((todo, index) => (
-    <li key={`${todo._id}-${index}`}>
-      <input
-        type="checkbox"
-        checked={todo.completed}
-        onChange={() => handleToggleTodo(todo._id)}
-        className="checkbox-label"
-      />
-      {editTodo && editTodo._id === todo._id ? (
-        <input
-          type="text"
-          value={editTodo.item_text}
-          onChange={(event) =>
-            setEditTodo({
-              ...editTodo,
-              item_text: event.target.value,
-            })
-          }
-        />
-      ) : (
-        <span>
-          {todo.item_text} - [{todo.completed ? "completed" : "active"}]
-        </span>
-      )}
-      {editTodo && editTodo._id === todo._id ? (
-        <button onClick={handleUpdateTodo}>Update</button>
-      ) : (
-        <button onClick={() => handleEditTodo(todo._id)}>Edit</button>
-      )}
-      <button onClick={() => handleDeleteTodo(todo._id)}>Delete</button>
-    </li>
-  ))}
-</ol>
-
+            {filteredTodos.map((todo) => (
+              <li key={todo.todo_id} data-todo-id={todo.todo_id}>
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={() => handleToggleTodo(todo.todo_id)}
+                  className="checkbox-label"
+                />
+                {editTodo === todo.todo_id ? (
+                  <input
+                    type="text"
+                    value={todo.item_text}
+                    onChange={(event) =>
+                      setTodos((prevTodos) =>
+                        prevTodos.map((t) =>
+                          t.todo_id === todo.todo_id
+                            ? { ...t, item_text: event.target.value }
+                            : t
+                        )
+                      )
+                    }
+                    onKeyDown={(event) => {
+                      if (event.key === "Enter") {
+                        handleUpdateTodo();
+                      }
+                    }}
+                  />
+                ): (
+                  <span>
+                    {todo.item_text} - [{todo.completed ? "completed" : "active"}]{" "}
+                    <span style={{ display: "none" }}>{todo.todo_id}</span>
+                  </span>
+                )}
+                {editTodo === todo.todo_id ? (
+                  <button onClick={handleUpdateTodo}>Update</button>
+                ) : (
+                  <button onClick={() => handleEditTodo(todo.todo_id)}>Edit</button>
+                )}
+                <button onClick={() => handleDeleteTodo(todo.todo_id, todo.item_text)}>
+                  Delete
+                </button>
+              </li>
+            ))}
+          </ol>
         </div>
       </div>
     </div>
